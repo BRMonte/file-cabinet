f3f7d0a4b2f04ad23435daaff1284eb4100b47af
bruno montebello <brunormontebello@hotmail.com>
2020-04-05 14:31:37 -0300

    Change root of application for autheticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'
+  root 'welcome#index' #when user IS NOT logged in: will be sent to this route/view
+
+  authenticated :user do
+    root "docs#index", as: "authenticated_root" #when user logged in: will be sent to this route/view
+  end
 
   resources :docs
 
